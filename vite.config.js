c71f7bd0730f120887b7d215ebc8d277d610190e
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-04-08 12:42:03 +0800

    feat: 🎸 local ssl

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const fs = require('fs')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(`${__dirname}/src/assets/localhost-key.pem`),
+      cert: fs.readFileSync(`${__dirname}/src/assets/localhost.pem`),
+    },
+  },
 })
