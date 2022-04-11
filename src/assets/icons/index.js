c4cd62577b03c54d1f43afada8d7c67d15b5141e
kevinhws <kevinshu1995@gmail.com>
2022-04-12 02:16:37 +0800

    feat: 🎸 logout svg

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,7 +1,9 @@
 import { ReactComponent as Adjustments } from './adjustments.svg'
 import { ReactComponent as User } from './user.svg'
+import { ReactComponent as Logout } from './logout.svg'
 
 export default {
   Adjustments,
   User,
+  Logout,
 }
