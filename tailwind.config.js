8ccd34c71c46ee7c50c12d0d676481808ca2c9de
kevinhws <kevinshu1995@gmail.com>
2022-04-19 11:13:01 +0800

    feat: 🎸 basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+    },
   },
   plugins: [],
 }
