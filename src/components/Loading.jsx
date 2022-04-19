795f906e35cc99903df9703538e6a0aca11993e4
kevinhws <kevinshu1995@gmail.com>
2022-04-19 11:13:31 +0800

    feat: 🎸 useLoading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="min-w-screen fixed top-0 left-0 h-full min-h-screen w-full bg-gray-200 text-gray-600">
+    <div className="min-w-screen fixed top-0 left-0 z-50 h-full min-h-screen w-full bg-gray-200 bg-opacity-75 text-gray-600">
       <div className="flex h-full w-full flex-col items-center justify-center gap-10">
         <div className="relative h-10 w-10 animate-ping">
           <span className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2">
--- src/hooks/useLoading.jsx
+++ src/hooks/useLoading.jsx
@@ -1,4 +1,5 @@
 import Loading from '/src/components/Loading.jsx'
+import { Transition } from '@headlessui/react'
 
 import { createContext, useContext, useState } from 'react'
 
@@ -6,11 +7,22 @@ export const LoadingContext = createContext()
 
 export function LoadingProvider({ children }) {
   const [isLoading, setIsLoading] = useState(false)
-  const value = { setIsLoading }
+  const value = { isLoading, setIsLoading }
 
   return (
     <LoadingContext.Provider value={value}>
-      {isLoading ? <Loading /> : children}
+      <Transition
+        show={isLoading === true}
+        enter="transition-opacity duration-75"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-150"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <Loading />
+      </Transition>
+      {children}
     </LoadingContext.Provider>
   )
 }
