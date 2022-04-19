246b3bd557a6a38d7bf45aa6973119073e35b5c9
kevinhws <kevinshu1995@gmail.com>
2022-04-19 11:11:23 +0800

    feat: 🎸 svg icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,10 +2,12 @@ import { ReactComponent as Adjustments } from './adjustments.svg'
 import { ReactComponent as User } from './user.svg'
 import { ReactComponent as Logout } from './logout.svg'
 import { ReactComponent as Mail } from './mail.svg'
+import { ReactComponent as Pencil } from './pencil.svg'
 
 export default {
   Adjustments,
   User,
   Logout,
   Mail,
+  Pencil,
 }
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,6 +1,6 @@
 import IconComponent from '/src/assets/icons/index.js'
 
-export default function Icon({ icon }) {
+export default function Icon({ icon, className }) {
   const Comp = IconComponent[icon]
-  return <Comp />
+  return <Comp className={className} />
 }
